62cbc4e65cf11a7289d21e70218a6201d0851508
teoyo <bigbang723@naver.com>
2024-05-02 17:15:13 +0900

    [update] : js 파일 주석 제거

--- js/main.js
+++ js/main.js
@@ -33,20 +33,3 @@ let slideShow = setInterval(function(){
     }
     console.log('무한');
     }, 7000);
-
-// let slideShow = setInterval(function(){
-//     for(let 지금사진 = 1; 지금사진 < 4; 지금사진 ++){
-//         if(지금사진 === 1){
-//             $('.slide-container').css('transform', `translateX(-100vw)`);
-//             지금사진 += 1;
-//         } else if(지금사진 === 2){
-//             $('.slide-container').css('transform', `translateX(-200vw)`);
-//             지금사진 += 1;
-//         } else if(지금사진 === 3){
-//             $('.slide-container').css('transform', `translateX(-0vw)`);
-//             지금사진 += 1;
-//         }
-//         console.log('무한');
-//         continue;
-//     }
-//     }, 7000);
