8704ae256a2266b6664a960069c6a1304aba07b0
teoyo <bigbang723@naver.com>
2024-05-02 15:29:57 +0900

    [update] : slide 기능 구현

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+let 지금사진 = 1;
+$('.next-btn').on('click', function(){
+    if(지금사진 === 1){
+        $('.slide-container').css('transform', `translateX(-100vw)`);
+        지금사진 += 1;
+    } else if(지금사진 === 2){
+        $('.slide-container').css('transform', `translateX(-200vw)`);
+        지금사진 += 1;
+    }
+});
+
+$('.prev-btn').on('click', function(){
+    if(지금사진 === 2){
+        $('.slide-container').css('transform', `translateX(0vw)`);
+        지금사진 -= 1;
+    } else if(지금사진 === 3){
+        $('.slide-container').css('transform', `translateX(-100vw)`);
+        지금사진 -= 1;
+    }
+});
