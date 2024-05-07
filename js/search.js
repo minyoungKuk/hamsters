c008dcd7a5d84cf4f72e61c348115c894d568265
teoyo <bigbang723@naver.com>
2024-05-07 15:39:07 +0900

    [update] : search.html 내 검색 기능 구현

--- js/search.js
+++ js/search.js
@@ -1,6 +1,16 @@
-let params = new URLSearchParams(window.location.search) // 주소창 주소 가져오기
-let keyword = params.get('keyword'); // 주소의 keyword 값 가져오기
-document.querySelector('.search-text-value').append(keyword); // html에 keyword 값 추가
+// 검색 버튼 누르면 querystring으로 현재 주소를 "주소?keyword=검색어"로 변경
+document.querySelector('.search-btn').addEventListener('click', function(){
+    if(document.querySelector('.search-input').value === ""){ 
+        alert('검색어를 입력해주세요.');
+    } else if(document.querySelector('.search-input').value !== "") {
+    let keyword = document.querySelector('.search-input').value; 
+    window.location.href=`/pages/search.html?keyword=${keyword}`; 
+    }
+})
+
+let params = new URLSearchParams(window.location.search) 
+let keyword = params.get('keyword'); 
+document.querySelector('.search-text-value').append(keyword); 
 
 // TMDB 영화 리스트 가져오기
 const apiKey = '4c329d16b4a318497a30199297839bb6';
@@ -11,12 +21,15 @@ const options = {
       accept: 'application/json',
       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YzMyOWQxNmI0YTMxODQ5N2EzMDE5OTI5NzgzOWJiNiIsInN1YiI6IjY2MjY5MDI5YjlhMGJkMDE2MWQ1YmQzNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.DVGQRZTSfphqw6UNo4rQYzhsF-C3GzH4apmBXzDPC0g'
     }
-  };
+};
   
-  fetch('https://api.themoviedb.org/3/movie/top_rated?language=ko-KR&page=1', options)
+function searchData (val) {
+    fetch(`https://api.themoviedb.org/3/search/movie?language=ko-KR&page=1&query=${val}&api_key=${apiKey}`, options)
     .then(response => response.json())
     .then(response => {
-        let movieList = response['results']
+        document.querySelector('.movie-card').innerHTML = "";
+
+        let movieList = response['results'];
         console.log(movieList);
         movieList.forEach(element => {
       
@@ -24,17 +37,25 @@ const options = {
             let movieImg = element['poster_path'];
             let card = `
             <div class="movie-card">
-            <img class="movie-img" src="${movieImg}" alt="영화 포스터">
+            <img class="movie-img" src="https://image.tmdb.org/t/p/w500${movieImg}" alt="${movieTitle}">
             <p class="movie-title">${movieTitle}</p>
             </div>
             `;
 
+            document.querySelector('.search-result').innerHTML += card; 
 
         });
     })
     .catch(err => console.error(alert('오류가 발생했습니다. 다시 시도해주세요.')));
+}
 
-    // 검색된 영화 개수 표시하기
-    let movieNum = document.querySelectorAll('.movie-card').length;
-    console.log(movieNum);
-    document.querySelector('.search-text-num').append(`${movieNum}`);
+// 검색어 필터링 하기
+document.querySelector('.search-btn').addEventListener('click', function(){
+    const val = document.querySelector('.search-input').value;
+    searchData(val);
+})
+
+// 검색된 영화 개수 표시하기
+let movieNum = document.querySelectorAll('.movie-card').length;
+console.log(movieNum);
+document.querySelector('.search-text-num').append(`${movieNum}`);
