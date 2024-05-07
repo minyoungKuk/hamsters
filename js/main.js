247a3635a427275ce3ca6d00ca93b9f6d0b87099
teoyo <bigbang723@naver.com>
2024-05-07 11:49:01 +0900

    [update] : 검색버튼 안눌리는 오류 수정

--- js/main.js
+++ js/main.js
@@ -1,27 +1,27 @@
 // 검색창
-let searchTitle = function(){
+// let searchTitle = function(){
     
-}
+// }
 
-// main - slide 넘기기
+// main - slide 클릭하여 넘기기
 let showNow = 1;
-$('.next-btn').on('click', function(){
+document.querySelector('.next-btn').addEventListener('click', function(){
     if(showNow === 1){
-        $('.slide-container').css('transform', `translateX(-100vw)`);
+        document.querySelector('.slide-container').style.transform = 'translateX(-100vw)';
         showNow += 1;
     } else if(showNow === 2){
-        $('.slide-container').css('transform', `translateX(-200vw)`);
+        document.querySelector('.slide-container').style.transform = 'translateX(-200vw)';
         showNow += 1;
     }
 });
 
 
-$('.prev-btn').on('click', function(){
+document.querySelector('.prev-btn').addEventListener('click', function(){
     if(showNow === 2){
-        $('.slide-container').css('transform', `translateX(0vw)`);
+        document.querySelector('.slide-container').style.transform = 'translateX(0vw)';
         showNow -= 1;
     } else if(showNow === 3){
-        $('.slide-container').css('transform', `translateX(-100vw)`);
+        document.querySelector('.slide-container').style.transform = 'translateX(-100vw)';
         showNow -= 1;
     }
 });
@@ -29,25 +29,25 @@ $('.prev-btn').on('click', function(){
 // main- slide 자동으로 넘어가기
 let slideShow = setInterval(function(){
     if(showNow === 1){
-        $('.slide-container').css('transform', `translateX(-100vw)`);
+        document.querySelector('.slide-container').style.transform = 'translateX(-100vw)';
         showNow += 1;
     } else if(showNow === 2){
-        $('.slide-container').css('transform', `translateX(-200vw)`);
+        document.querySelector('.slide-container').style.transform = 'translateX(-200vw)';
         showNow += 1;
     } else if(showNow === 3){
-        $('.slide-container').css('transform', `translateX(-0vw)`);
+        document.querySelector('.slide-container').style.transform = 'translateX(0vw)';
         showNow = 1;
     }
 }, 7000);
 
 // 검색 버튼 누르면 querystring으로 현재 주소를 "주소?keyword=검색어"로 변경
-$('.search-btn').click(function(){
-    let keyword = $('.search-input').val(); // 사용자가 쓴 검색어 가져오기
+document.querySelector('.search-btn').addEventListener('click', function(){
+    let keyword = document.querySelector('.search-input').value; // 사용자가 쓴 검색어 가져오기
     window.location.href=`/pages/search.html?keyword=${keyword}`; // 주소 바꾸기
 })
 
 
 // footer
-$('.custmer-center').click(function(){
+document.querySelector('.custmer-center').addEventListener('click', function(){
     alert('고객센터는 준비중입니다.');
 })
